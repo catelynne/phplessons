d171774f447370f2f956e95b7334480f7594f154
catelynne donnell <110991600+catelynne@users.noreply.github.com>
2024-03-25 08:15:22 -0500

    grngnj

---
+++ operators.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+  <?php
+ //video 8
+    //Arithmetic Operators
+    echo 5**2;
+    echo 8%5;
+
+
+    ?>
+
+    <?php
+//video 9
+    //Assignment Operators
+    $x = 100;
+    $x += 20;
+
+    echo $x;
+
+?>
+
+    <?php
+//video 10
+    //Comparison Operators
+    $x = 10;
+    $y = 10;
+
+    if ($x <> $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+
+
+
+?>
+
+    <?php
+//video 11
+    //Increment/Decrement Operators
+    $x = 10;
+    echo $x--;
+    echo $x;
+
+?>
+
+    <?php
+//video 12
+    //Logical Operators
+    $x = 10;
+    $y = 20;
+
+    if ($x == $y xor 1 == 1) {
+        echo "True";
+    }
+
+?>
+</body>
+</html>
